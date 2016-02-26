dc3e2071d38ac4aa17a8d3b918b664e80b502532
grifcj <grifcj@gmail.com>
2016-02-26 15:56:11 -0800

    Add install script. For now make it really simple and avoid all the safety fluff. Add zenburn colors. Add ignore file.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+################################################################################
+# Install dotfiles to system.
+#
+# TODO Currently, this only supports unix systems, obviously. Consider making
+# it a python script to allow better support of Windows.
+################################################################################
+
+dir=~/dotfiles
+
+ln -svf $dir/bashrc ~/.bashrc
+ln -svf $dir/vimrc ~/.vimrc
+mkdir -pv ~/.vim/colors
+ln -svf $dir/colors/zenburn.vim ~/.vim/colors/zenburn.vim
